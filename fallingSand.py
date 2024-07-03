d3042848a04b3a346da06288518e7c68d870d941
David Kessler <dkesslerm03@correo.ugr.es>
2024-07-03 15:35:08 +0200

    Grass and falling sand implemented

--- fallingSand.py
+++ fallingSand.py
@@ -10,16 +10,23 @@ def zerosGrid(width, height):
 
 # Initialize Pygame
 pygame.init()
+clock = pygame.time.Clock()
 
 WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GREY = (120, 120, 120)
+GREEN = (0, 255, 0)
 sandWidth = 10
-width, height = 800, 800
+width, height = 800,800
 rows = int(width/sandWidth)
 cols = int(height/sandWidth)
 window = pygame.display.set_mode((width, height))
+
+pygame.display.set_caption("Falling Sand - David Kessler")
 grid = np.zeros((rows, cols))
 
 running = True
+cont = 0
 while running:
     eventList = pygame.event.get()
     for event in eventList:
@@ -28,26 +35,30 @@ while running:
         elif event.type == pygame.MOUSEBUTTONDOWN:
             x,y = pygame.mouse.get_pos()
             grid[int(x/sandWidth)][int(y/sandWidth)] = 1
-    
-    for i in range(rows):
-        for j in range(cols):
-            pygame.draw.rect(window, WHITE,(i*sandWidth, j*sandWidth, sandWidth , sandWidth), 1)
 
-            if(grid[i][j] == 1):
-                pygame.draw.rect(window, WHITE, (i*sandWidth, j*sandWidth, 10 , 10))
+    for i in range(cols):
+        for j in range(rows):
+            pygame.draw.rect(window, (grid[i][j]*255, grid[i][j]*255, grid[i][j]*255), (i*sandWidth, j*sandWidth, sandWidth, sandWidth))
+            pygame.draw.rect(window, GREY,(i*sandWidth, j*sandWidth, sandWidth , sandWidth), 1)
 
-    # (grid[i][j]*255,grid[i][j]*255,grid[i][j]*255)
-    # auxGrid = zerosGrid(width, height)
-    # for i in range(rows):
-    #     for j in range(cols):
-    #         if (grid[i][j] == 1):
-    #             if (grid[i][j+1] == 0):
-    #                 auxGrid[i][j] = 0
-    #                 auxGrid[i][j+1] = 1
+            
+    auxGrid = np.zeros((rows, cols))
+    for i in range(cols):
+        for j in range(rows):
+            if (grid[i][j] == 1):
+                if (grid[i][j+1] == 0  and j < rows - 2):
+                    auxGrid[i][j+1] = 1
+                else:
+                    auxGrid[i][j] = 1
+            elif (j== rows-1):
+                pygame.draw.rect(window, GREEN, (i*sandWidth, j*sandWidth, sandWidth, sandWidth))
+                pygame.draw.rect(window, GREY,(i*sandWidth, j*sandWidth, sandWidth , sandWidth), 1)
 
+            
+            
+    grid = auxGrid
 
-    # grid = auxGrid
-
+    clock.tick(30)
     pygame.display.update()
 
 pygame.quit()
